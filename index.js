723115976fc8f15085504fb5b7e4e874f69e166e
Raihan Rifaldi <raihanrifaldiii@gmail.com>
2024-03-06 14:22:10 +0700

    first upload

---
+++ index.js
@@ -0,0 +1,9 @@
+
+        // Example of JavaScript manipulation
+        const dropdown = document.createElement('select');
+        dropdown.innerHTML = `
+            <option value="">Select an option</option>
+            <option value="option1">Option 1</option>
+            <option value="option2">Option 2</option>
+        `;
+        document.body.appendChild(dropdown);
